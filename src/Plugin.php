27f1d342e98fe07a2230da018a4901ae8e18f816
ronlim <ron.lim@thepixelage.com>
2022-05-06 07:17:58 +0800

    Fix bug caused by adding custom arguments when GQL schema does not allow entries or products to be queried [#1]

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,6 +8,7 @@ use craft\base\Element;
 use craft\base\Model;
 use craft\commerce\elements\db\ProductQuery;
 use craft\commerce\elements\Product;
+use craft\commerce\helpers\Gql as CommerceGqlhelper;
 use craft\db\Query;
 use craft\elements\conditions\ElementCondition;
 use craft\elements\db\ElementQuery;
@@ -26,6 +27,7 @@ use craft\gql\TypeManager;
 use craft\gql\types\DateTime;
 use craft\helpers\Cp;
 use craft\helpers\DateTimeHelper;
+use craft\helpers\Gql as GqlHelper;
 use craft\helpers\Html;
 use craft\services\Gql;
 use Exception;
@@ -299,16 +301,21 @@ class Plugin extends \craft\base\Plugin
             Gql::class,
             Gql::EVENT_REGISTER_GQL_QUERIES,
             function(RegisterGqlQueriesEvent $event) {
-                $event->queries['entries']['args']['markedAsNew'] = [
-                    'name' => 'markedAsNew',
-                    'type' => Type::boolean(),
-                    'description' => 'Narrows the query results to only entries that are marked as new.'
-                ];
-                $event->queries['products']['args']['markedAsNew'] = [
-                    'name' => 'markedAsNew',
-                    'type' => Type::boolean(),
-                    'description' => 'Narrows the query results to only products that are marked as new.'
-                ];
+                if (GqlHelper::canQueryEntries()) {
+                    $event->queries['entries']['args']['markedAsNew'] = [
+                        'name' => 'markedAsNew',
+                        'type' => Type::boolean(),
+                        'description' => 'Narrows the query results to only entries that are marked as new.'
+                    ];
+                }
+
+                if (CommerceGqlhelper::canQueryProducts()) {
+                    $event->queries['products']['args']['markedAsNew'] = [
+                        'name' => 'markedAsNew',
+                        'type' => Type::boolean(),
+                        'description' => 'Narrows the query results to only products that are marked as new.'
+                    ];
+                }
             }
         );
     }
